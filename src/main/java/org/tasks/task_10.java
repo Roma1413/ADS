9e08d1baae9664e23bb2e60f5c6d230faa0bb97c
Ramazan Arinov <arinovramazan0101@gmail.com>
2025-07-13 21:51:37 +0500

    add files

--- src/main/java/org/tasks/task_10.java
+++ src/main/java/org/tasks/task_10.java
@@ -7,6 +7,6 @@ public class task_10 {
      */
     public static int gcd(int a, int b) {
         if (b == 0) return a;
-        return gcd(b, a % b);
+        return gcd(b, a % b); //function to get gcd
     }
 }
